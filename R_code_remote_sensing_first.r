7895eb79984bd4949c4d7521d0d9c081c45ac1e4
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-03-17 13:03:08 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,6 +1,15 @@
 # Il mio primo codice in R per il telerilevamento
 
-setwd("/Users/benedettabellini/lab/")
 library(raster)
+setwd("/Users/benedettabellini/lab/")
+
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 plot(p224r63_2011)
+
+# colour change
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(p224r63_2011, col=cl)
+
+# colour change -> new
+cl <- colorRampPalette(c("purple","green","magenta"))(100)
+plot(p224r63_2011, col=cl)
