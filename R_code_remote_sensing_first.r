d40f205fbfa367756ad6f5af8f04b4a3c80867fd
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-03-21 13:34:45 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -3,6 +3,7 @@
 library(raster)
 setwd("/Users/benedettabellini/lab/")
 
+# Importo l'immagine satellitare e la visualizzo
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 plot(p224r63_2011)
 
@@ -10,6 +11,6 @@ plot(p224r63_2011)
 cl <- colorRampPalette(c("black","grey","light grey"))(100)
 plot(p224r63_2011, col=cl)
 
-# colour change -> new
+# colour change -> new 
 cl <- colorRampPalette(c("purple","green","magenta"))(100)
 plot(p224r63_2011, col=cl)
