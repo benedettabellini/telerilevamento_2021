60defd1a0d488dc685e5deaf69afc814489f6e2a
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-03-31 13:01:10 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,7 @@
+# Time series analysis
+# Greenland increase of temperature
+# Data and code from Emanuela Cosma
+
+# install.packages("raster")
+library(raster)
+setwd("/Users/benedettabellini/lab/greenland/")
