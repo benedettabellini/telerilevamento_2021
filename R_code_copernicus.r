824913bd6a6cd9171a1a27dde000cd500a5a8aa8
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-04-15 10:12:44 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -1,13 +1,13 @@
 # R_code_copernicus.r
 # Visualizing Copernicus data
 
-instal.packages("ncdf4") # installo il pacchetto "ncdf4"
+install.packages("ncdf4") # installo il pacchetto "ncdf4"
 library(raster)
 library(ncdf4) # richiamo il pacchetto installato
 
 setwd("/Users/benedettabellini/lab") # setto cartella di lavoro
 
-albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAC_V!.5.1.nc") # con raster carico un singolo strato
+albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAV_V1.5.1.nc") # con raster carico un singolo strato
 
 cl <- colorRampPalette(c('light blue','green','red','yellow'))(100)
 plot(albedo, col=cl)
