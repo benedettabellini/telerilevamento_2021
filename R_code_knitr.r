0398bf4aad285494ab5698e91622d497c9737737
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-04-16 10:07:19 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -4,4 +4,7 @@ setwd("/Users/benedettabellini/lab/") #setto cartella di lavoro
 
 library(knitr) # richiamo il pacchetto knitr
 
+install.packages("tinytex") 
+library(tinytex)
+
 stitch("R_code_greenland.rtf", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
