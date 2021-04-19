9780053bd77467fbcc61fee4b9a992f10b7aa6cd
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-04-19 14:25:21 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -7,4 +7,5 @@ library(knitr) # richiamo il pacchetto knitr
 install.packages("tinytex") #installo pacchetto tinytex
 library(tinytex) # richiamo il pacchetto
 
+
 stitch("R_code_greenland.r.rtf", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
