6b441c88059ede6489890e94a207333667b6c87d
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-04-21 11:22:51 +0200

    Create R_code_classification.r

---
+++ R_code_classification.r
@@ -0,0 +1,8 @@
+#R_code_classification
+
+library(raster) #richiamo il pacchetto rastercode_classification
+
+setwd("/Users/benedettabellini/lab/") #setto la cartella di lavoro
+
+
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg") #importo su R l'immagine
