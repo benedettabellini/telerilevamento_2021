01a14633b3179ef84f6ca111f0c621ee5f8cfc6e
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-04-21 12:09:00 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,8 +1,20 @@
 #R_code_classification
 
-library(raster) #richiamo il pacchetto rastercode_classification
+library(raster) #richiamo il pacchetto raster
+library(RStoolbox) #richiamo il pacchetto RStoolbox
 
 setwd("/Users/benedettabellini/lab/") #setto la cartella di lavoro
 
-
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg") #importo su R l'immagine
+so #visualizzo i dati dell'immagine
+
+# visualize RGB levels
+plotRGB(so, 1, 2, 3, stretch="lin")
+
+# Unsupervised Classification
+soc <- unsuperClass(so, nClasses=3)
+plot(soc$map) #visualizzo l'immagine 
+
+# # Unsupervised Classification with 20 classes
+soc20 <- unsuperClass(so, nClasses=20)
+plot(soc20$map)
