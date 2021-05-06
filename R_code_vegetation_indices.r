62a6f2d3a98cdf0eb263a8f343341b5e972c1528
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-05-06 23:50:02 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -1,7 +1,10 @@
 # R_code_vegetation_indices.r
 
 library(raster) #richiamo il pacchetto 
-library(RStoolbox) # richiamo il paccheto
+library(RStoolbox) # richiamo il pacchetto per calcolare gli ndvi
+install.packages("rasterdiv") #carico il pacchetto rasterdiv per calcolorare gli ndvi del mondo
+library(rasterdiv) # richiamo il pacchetto
+library(rasterVis)
 
 setwd("/Users/benedettabellini/lab/") # setto la cartella di lavoro
 
@@ -59,5 +62,19 @@ plot(vi1, col=cl) # visualizzo gli indici calcolati
 
 vi2 <- spectralIndices(defor2, green=3, red=2, nir=1) # calcolo tutti gli indici
 plot(vi2, col=cl)
+
+# worldwide NDVI 
+plot(copNDVI)
+
+# trasformo i pixel con valori dei pixel 253, 254, 255 (acqua) in NA
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA)) 
+plot(copNDVI)
+
+# raster package needed:
+levelplot(copNDVI)
+
+
+
+
 
 
