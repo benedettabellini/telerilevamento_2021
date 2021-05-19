6be6c941dff74802c87d82c88fee69ec257b7bd3
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-05-19 12:49:39 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -18,7 +18,7 @@ cl <- colorRampPalette(c('black','white','red','magenta','green'))(100) #creo sc
 plot(ndvi,col=cl) #plotto l'indice di vegetazione con scala di colori creata
 
 # Calcolo della deviazione standard dell'ndvi
-ndvisd3 <- focal(ndvi, w=matrix(1/9,nrow=3,ncol=3), fun=sd)
+ndvisd3 <- focal(ndvi, w=matrix(1/9,nrow=3,ncol=3), fun=sd) #con moving window 3x3
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #creo scala di colori
 plot(ndvisd3, col=clsd) #plotto
 
@@ -27,7 +27,19 @@ ndvimean3 <- focal(ndvi, w=matrix(1/9,nrow=3,ncol=3), fun=mean)
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #creo scala di colori
 plot(ndvimean3, col=clsd) #plotto
 
-# Cambio grandezza della moving window 13x13
+# Uso moving window 13x13
 ndvisd13 <- focal(ndvi, w=matrix(1/169,nrow=13,ncol=13), fun=sd) #calcolo devizione standard
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #creo scala di colori
 plot(ndvisd13, col=clsd) #plotto
+
+# Uso moving window 5x5
+ndvisd5 <- focal(ndvi, w=matrix(1/25,nrow=5,ncol=5), fun=sd) #calcolo devizione standard
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) #creo scala di colori
+plot(ndvisd5, col=clsd) #plotto
+
+# PCA 
+sentpca <- rasterPCA(sent)
+plot(sentpca$map)
+summary(sentpca$model)
+#the first PC contains 67.3604% of the original information
+
