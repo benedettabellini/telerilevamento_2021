5234ed947d2942f3cf4c350d7a5202e4127fa7aa
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-12-26 18:25:36 +0100

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -76,7 +76,7 @@ ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="
 
 p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
 p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
-grid.arrange(p1, p2, nrow=1) # plotto i due ggplot di fianco 
+grid.arrange(p1, p2, nrow=1) # plotto i due ggplot di fianco  
 
 
 
