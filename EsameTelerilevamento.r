60cc8bf4e860a3a11e4c90c62179c3274810a6c4
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-12-28 23:08:37 +0100

    Update EsameTelerilevamento.r

--- EsameTelerilevamento.r
+++ EsameTelerilevamento.r
@@ -4,11 +4,14 @@
 install.packages("raster") #installo il pacchetto raster 
 install.packages("rasterVIs") #installo il pacchetto rasterVis
 install.packages("RStoolbox") #installo il pacchetto RStoolbox
+install.packages("rasterdiv") #installo il pacchetto rasterdiv
 
 #Richiamo i pacchetti installati in R
 library(raster) 
 library(rasterVis) 
 library(RStoolbox) 
+library(rasterdiv)
+
 setwd("/Users/benedettabellini/sardegna") #setto la cartella di lavoro 
 
 #Immagine del 16/09/2019
@@ -23,39 +26,48 @@ Sard2021 <- stack(import2)
 
 # presento le due immagini nel visibile, e le visualizzo in una finestra grafica 1x2
 par(mfrow=c(1,2)) # la funzione <par> crea una finestra grafica
-plotRGB(Sard2019, 3,2,1, stretch="lin") #la funzione <plotRGB> permette di visualizzare 3 bande alla volta
+plotRGB(Sard2019, 3,2,1, stretch="lin") #la funzione <plotRGB> permette di visualizzare 3 bande alla volta, con uno stretch lineare
 plotRGB(Sard2021, 3,2,1, stretch="lin")
 
-# presento le due immagini in falsi colori, e le visualizzo in una finestra grafica 1x2
-par(mfrow=c(1,2)) # con 
-plotRGB(Sard2019, 4,3,1, stretch="lin")
-plotRGB(Sard2021, 4,3,1, stretch="lin")
+# presento le due immagini in falsi colori (con banda NIR nella componente red), e le visualizzo in una finestra grafica 1x2
+par(mfrow=c(1,2)) 
+plotRGB(Sard2019, 4,3,2, stretch="lin")
+plotRGB(Sard2021, 4,3,2, stretch="lin")
 
-#Calcolo l'ndvi 
-#ndvi=(NIR-RED)/(NIR+RED)
+# Visualizzo sia le immagini nel visibile che in falsi colori in una finestra 2x2
+par(mfrow=c(2,2)) 
+plotRGB(Sard2019, 3,2,1, stretch="lin") 
+plotRGB(Sard2021, 3,2,1, stretch="lin")
+plotRGB(Sard2019, 4,3,2, stretch="lin")
+plotRGB(Sard2021, 4,3,2, stretch="lin")
+
+#Bande Sentinel
+#B04: rosso
+#B08: nir
+
+cl <- colorRampPalette(c("darkblue","yellow","red","black"))(100) #creo una scala di colori per visualizzare le immagini che creerò 
+
+#Calcolo l'ndvi=(NIR-RED)/(NIR+RED)
 #ndvi 2019
-red19 <- raster("T32TMK_20190916T101029_10m_B04.jp2")
-nir19 <- brick("T32TMK_20190916T101029_10m_B08.jp2") 
-ndvi19 <- (nir19-red19)/(nir19+red19)
-plot(ndvi19)
-
-#ndvi21
-red21 <- brick("T32TMK_20210905T100549_10m_B04.jp2")
-nir21 <- brick("T32TMK_20210905T100549_10m_B08.jp2")
-ndvi21 <- (nir21-red21)/(nir21+red21)
-plot(ndvi21)
-
-# Visualizzo i due ndvi con una colorRampPalette
-cl <- colorRampPalette(c("black","green","red"))(100)
+red19 <- raster("T32TMK_20190916T101029_10m_B04.tif") #con <raster> carico la banda B4 e la associo ad una variabile
+nir19 <- raster("T32TMK_20190916T101029_10m_B08.tif") 
+ndvi19 <- (nir19-red19)/(nir19+red19) #calcolo l'indice di vegetazione dell'anno 2019
+#ndvi 2021
+red21 <- brick("T32TMK_20210905T100549_10m_B04.tif")
+nir21 <- brick("T32TMK_20210905T100549_10m_B08.tif")
+ndvi21 <- (nir21-red21)/(nir21+red21) #calcolo l'indice di vegetazione dell'anno 2021
+#Visualizzo i due ndvi in una finestra 1x2
 par(mfrow=c(1,2))
-plot(ndvi19, col=cl)
-plot(ndvi21, col=cl)
+plot(ndvi19, col=cl, main="NDVI 16/09/2019")
+plot(ndvi21, col=cl, main="NDV 05/09/2021")
 
-
-cl <- colorRampPalette(c("black","dark red","orange","pink","green"))(100)
+#Differenza fra i due ndvi 
+difndvi <- ndvi19-ndvi21
+cld <- colorRampPalette(c("blue","white","red","black"))(100)#creo una nuova scala di colori
+plot(difndvi, col=cld) #plotto la differenza fra i due ndvi con la nuova scala di colori 
 
 # cambio risoluzione all'immagine nir da 10m a 20 m 
-swir19 <- brick("T32TMK_20190916T101029_B12.jp2") 
+swir19 <- raster("T32TMK_20190916T101029_B12.tif") 
 nir19_20m<- aggregate(nir19, fact=2)
 writeRaster(nir19,"T32TMK_20190916T101029_20m_B12.tif")
 
