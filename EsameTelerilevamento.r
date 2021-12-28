ffaff961e5b7faebd87f3641c81f20142b4a44b0
benedettabellini <80513558+benedettabellini@users.noreply.github.com>
2021-12-28 23:32:51 +0100

    Update EsameTelerilevamento.r

--- EsameTelerilevamento.r
+++ EsameTelerilevamento.r
@@ -44,6 +44,7 @@ plotRGB(Sard2021, 4,3,2, stretch="lin")
 #Bande Sentinel
 #B04: rosso
 #B08: nir
+#B12: swir
 
 cl <- colorRampPalette(c("darkblue","yellow","red","black"))(100) #creo una scala di colori per visualizzare le immagini che creerò 
 
@@ -67,24 +68,29 @@ cld <- colorRampPalette(c("blue","white","red","black"))(100)#creo una nuova sca
 plot(difndvi, col=cld) #plotto la differenza fra i due ndvi con la nuova scala di colori 
 # i valori più alti indicano dove ho maggior sofferenza della vegetazione
 
-# cambio risoluzione all'immagine nir da 10m a 20 m 
+#Calcolo NBR, prima devo fare delle procedure:
+#carico la banda SWIR, essa ha una risoluzione di 20 m 
 swir19 <- raster("T32TMK_20190916T101029_B12.tif") 
-nir19_20m<- aggregate(nir19, fact=2)
-writeRaster(nir19,"T32TMK_20190916T101029_20m_B12.tif")
-
+# cambio risoluzione all'immagine nir, sia del 2019 che del 2021, da 10m a 20 m 
+nir19_20m<- aggregate(nir19, fact=2) # all'immagine salvata in nir19 cambio moltiplico la risoluzione di 
+writeRaster(nir19_20m,"T32TMK_20190916T101029_20m_B08.tif") #salvo con la funzione <writeRaster> l'immagine appena creata nella cartella di lavoro 
 NBR19 <- (nir19_20m-swir19)/(nir19_20m+swir19) # calcolo l'NBR
 plot(NBR19, col=cl)
-swir21 <- brick("T32TMK_20210905T100549_B12.jp2") 
+#Eseguo lo stesso procedimento precidente all'immagine del 2021
+swir21 <- raster("T32TMK_20210905T100549_B12.tif") #carico la B12
 nir21_20m<- aggregate(nir21, fact=2)
+writeRaster(nir21_20m,"T32TMK_20210905T100549_20m_B08.tif" )
 NBR21 <- (nir21_20m-swir21)/(nir21_20m+swir21)
+#Visualizzo i due NBR in una finestra 1x2
 plot(NBR21, col=cl)
 par(mfrow=c(1,2))
-plot(NBR19, col=cl)
-plot(NBR21, col=cl)
+plot(NBR19, col=cl, main="NBR 16/09/2019")
+plot(NBR21, col=cl, main="NBR 05/09/2021")
+# Calcolo la differenza fra i due NBR 
 deltaNBR <- NBR19-NBR21
-plot(deltaNBR, col=cl)
+plot(deltaNBR, col=cld) #plotto la differenza fra i due NDR con la scala di colori precedentemente creata
 
-ndvi <- ()
+
 
 # Profilo spettrale
 
